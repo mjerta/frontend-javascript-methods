912f76844087cb84064f10c827e6d1727f2900ae
Maarten Postma <maarten.postma1000@gmail.com>
2024-06-23 22:50:05 +0200

    Add result to string-methods.js

--- string-methods.js
+++ string-methods.js
@@ -5,7 +5,6 @@
 const word = "zoetsappig";
 console.log(word.length);
 
-
 /* Opdracht 1 */
 //  Check of het woord "Banaan" langer is dan het woord "Aardappel" en log dit in de terminal.
 // ---- Verwachte uitkomst: false
@@ -13,7 +12,7 @@ console.log(word.length);
 const fruit = "Banaan";
 const vegetable = "Aardappel";
 
-
+console.log(fruit.length > vegetable.length);
 
 /* Opdracht 2 */
 //  Zorg ervoor dat de laatste letter van het woord in fruit en vegetable (hierboven) in de terminal gelogd wordt.
@@ -21,29 +20,28 @@ const vegetable = "Aardappel";
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
 
-
+console.log(fruit.charAt(fruit.length - 1));
+console.log(vegetable.charAt(vegetable.length - 1));
 
 /* Opdracht 3 */
 //  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
 // ---- Verwachte uitkomst: "De kat krabt de krullen van de trap"
 
 const userInput = "  De kat krabt de krullen van de trap ";
-
-
+console.log(userInput.trim());
 
 /* Opdracht 4 */
 // Check of het woord "Fantastisch" in onderstaande zin voorkomt en log dit in de terminal.
 // ---- Verwachte uitkomst: true
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
-
-
+console.log(story.includes("Fantastisch"));
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
-
-
+const arrStory = story.split(" ",7)
+console.log(arrStory.join(" "));
 
 /* Opdracht 6 */
 // Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
@@ -53,8 +51,4 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 // ---- Verwachte uitkomst: "€35,59"
 
 const price = "$35.59";
-
-
-
-
-
+console.log(price.replace("$","€"));
